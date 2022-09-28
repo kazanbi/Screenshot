52b5ea57760b0253ccd0ff51a8f13e6d49bc1944
Abdulaev Kazanbi <889login@gmail.com>
2022-09-28 16:32:09 +0300

    idk

--- js/script.js
+++ js/script.js
@@ -5,23 +5,6 @@ const key = 'k-l13yweaBiJusJsMpfHw0Wk-pwLDHMKb6_gpxlzQdA',
     
 let block = document.querySelector('.gallery__inner');
 
-//searchApi
-async function requestSearch(){
-    try{
-        const query =  document.querySelector('.search').value;
-        let res = await fetch(`${api}/search/photos/?client_id=${key}&query=${query}`);
-        let data = await res.json();
-        data.results.forEach(photo => {
-            block.innerHTML += `<div class="gallery__item">
-                <img src="${photo.urls.regular}" alt="">
-                <div class="gallery__item-text"><box-icon name='heart' type='solid'></box-icon></div>
-            </div>`;
-        });
-    }
-    catch(e){console.log('Error: ' + e); }   
-}
-
-searchBtn.addEventListener('click', requestSearch);
 
 //scroll to the top of the page
 window.addEventListener('scroll', () =>{
@@ -38,27 +21,75 @@ btn.addEventListener('click', () => {
 });
 
 //maconry
-let macy = Macy({
+let macyInstance = Macy({
     container: block,
     trueOrder: false,
     waitForImages: false,
     margin: 3,
 });
+function fixMancyBug(){
+    macyInstance.runOnImageLoad(function () {
+        macyInstance.recalculate(true, true);
+        var evt = document.createEvent('UIEvents'); 
+        evt.initUIEvent('resize', true, false, window, 0); 
+        window.dispatchEvent(evt);
+    }, true);
+}
 
 //api request
 async function request(){
     try{
         let response = await fetch(`${api}/photos/?client_id=${key}&per_page=50`);
         let data = await response.json();
-        let keyD;
-
-        for(keyD in data){
+        data.forEach(key => {
             block.innerHTML += `<div class="gallery__item">
-                <img src="${data[keyD].urls.regular}" alt="">
-                <div class="gallery__item-text"><box-icon name='heart' type='solid'></box-icon></div>
-            </div>`;
-        }
+                <img src="${key.urls.regular}" alt="">
+                <div class="gallery__item-text"><box-icon class="heart" name='heart' type='solid'></box-icon></div>
+            </div>`
+        });
+        fixMancyBug();
     }
     catch(e){console.log('Error: ' + e);}
 }
 request();
+
+//removePhoto
+function removePhoto(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//searchApi
+async function requestSearch(){
+    removePhoto(block);
+    try{
+        const query =  document.querySelector('.search').value;
+        let res = await fetch(`${api}/search/photos/?client_id=${key}&query=${query}&per_page=50`);
+        let data = await res.json();
+        if(!query) location.reload();
+        data.results.forEach(photo => {
+            block.innerHTML += `<div class="gallery__item">
+                <img src="${photo.urls.regular}" alt="">
+                <div class="gallery__item-text"><box-icon class="heart" name='heart' type='solid'></box-icon></div>
+            </div>`
+            console.log(photo);
+        });
+        fixMancyBug();
+    }
+    catch(e){
+        console.log('Error: ' + e); 
+        if(e === 'TypeError: Failed to fetch'){
+            alert('there is no such request');
+        }
+    }   
+}
+searchBtn.addEventListener('click', requestSearch);
+
+//-Favourites-
+
+let favBtn = document.querySelector('.heart');
+
+favBtn.addEventListener('click', () =>{
+
+});
